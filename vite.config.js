f67248dd2f8527be2ae5043f31c07086715f5418
Alson <alson.a@disprz.com>
2024-05-12 22:47:31 +0530

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// vite.config.js
-export default {
-  // ...
-  base: '/Portofolio/', // Replace with your repository name
-  // ...
-};
